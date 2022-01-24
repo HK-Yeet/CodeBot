083b173ddd3a6ae704a43fb0c5e3c6cc02bcb863
HK <68507957+HK-Yeet@users.noreply.github.com>
2022-01-24 11:42:40 -0700

    yes

--- src/commands/emoji.js
+++ src/commands/emoji.js
@@ -5,17 +5,17 @@ module.exports = {
     let emoji = message.guild.emojis.cache.find(emoji => emoji.name === text)
     if(!emoji) return message.reply("Could not find that emoji.")
     if (emoji.animated === true) {
-       let embed = new MessageEmbed()
+      let embed = new MessageEmbed()
         .setTitle(`${emoji.name}`)
-        .setImage(`${emoji.url}`)
-        .setThumbnail("```<a:" + emoji.name +":"+ emoji.id +">```")
-        message.channel.send({ embeds: [embed] });
-     } else {
-        let embed = new MessageEmbed()
+        .setThumbnail(`${emoji.url}`)
+        .setDescription("```<a:" + emoji.name +":"+ emoji.id +">```")
+      message.channel.send({ embeds: [embed] });
+    } else {
+      let embed = new MessageEmbed()
         .setTitle(`${emoji.name}`)
         .setThumbnail(`${emoji.url}`)
         .setDescription("```<" + emoji.name +":"+ emoji.id +">```")
-        message.channel.send({ embeds: [embed] });
-     }
+      message.channel.send({ embeds: [embed] });
+    }
   },
 };
